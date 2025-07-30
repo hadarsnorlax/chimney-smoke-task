ca02e97f0522721c9f6d44d5ba2db2cb279aa16b
hadarsnorlax <hadar.mirsky1@gmail.com>
2025-07-30 23:46:23 +0300

    Reduce repetition for cleaner code

--- smoke.py
+++ smoke.py
@@ -26,36 +26,34 @@ def smoke(current_time, prev_time, particles, params):
     particles_updated = copy.deepcopy(particles)
     delta_t = current_time - prev_time
 
-    _update_particles(particles_updated, delta_t, params)
-    _add_particles(particles_updated, current_time, delta_t, params)
+    for idx, particle in enumerate(particles_updated):
+        _update_particle(particle, idx, current_time, delta_t, params)
+
+    new_particles_amount = int(delta_t * params["particles_per_sec"])
+    for idx in range(len(particles), len(particles) + new_particles_amount):
+        particle = _create_particle(current_time, idx, params)
+        _update_particle(particle, idx, current_time, delta_t, params)
+        particles_updated.append(particle)
 
     return particles_updated
 
 
-def _update_particles(particles, delta_t, params):
-    for particle in particles:
-        _update_acceleration(particle, params)
-        initial_velocity = (particle["X"][1], particle["Y"][1], particle["Z"][1])
-        _update_velocity(particle, delta_t)
-        _update_location(particle, initial_velocity, delta_t)
+def _update_particle(particle, particle_idx, current_time, delta_t, params):
+    initial_velocity = (particle["X"][1], particle["Y"][1], particle["Z"][1])
+    _update_acceleration(particle, particle_idx, current_time, params)
+    _update_velocity(particle, delta_t)
+    _update_location(particle, initial_velocity, delta_t)
 
 
-def _add_particles(particles, current_time, delta_t, params):
-    new_particles_amount = int(delta_t * params["particles_per_sec"])
-    new_particles = []
+def _update_acceleration(particle, particle_idx, current_time, params):
+    g = _get_gravity_acceleration()
+    drag_a = _get_drag_acceleration(particle, params)
+    buoyancy_a = _get_buoyancy_acceleration(current_time, particle_idx, params)
 
-    for idx in range(len(particles), len(particles) + new_particles_amount):
-        rng = _get_rng_particle_seed(current_time, idx)
-        initial_loc = _get_random_chimney_loc(rng, params)
-        initial_v = _get_random_initial_velocity(rng, params)
-        particle = _create_particle(initial_loc, initial_v)
-
-        _update_acceleration(particle, params, delta_t)
-        _update_velocity(particle, delta_t)
-        _update_location(particle, initial_v, delta_t)
-        new_particles.append(particle)
-
-    particles += new_particles
+    particle["Z"][2] += g
+    particle["Z"][2] += buoyancy_a
+    particle["X"][2] += drag_a[0]
+    particle["Y"][2] += drag_a[1]
 
 
 def _get_gravity_acceleration():
@@ -64,7 +62,7 @@ def _get_gravity_acceleration():
     return g
 
 
-# in [m/sec^2], (x^,y^) direction
+# drag acceleration from wind in [m/sec^2], (x^,y^) direction
 def _get_drag_acceleration(particle, params):
     # v diff between particle and wind
     v_relative = (particle["X"][1] - params["wind_vel"][0], particle["Y"][1] - params["wind_vel"][1])
@@ -73,55 +71,48 @@ def _get_drag_acceleration(particle, params):
     return drag_a
 
 
-# use time_alive for new particles
-def _get_buoyancy_acceleration(particle, params, time_alive=None):
-    buoyancy_a = None # in [m/sec^2], z^ direction
-    if (time_alive):
-        # buoyancy_a = buoyancy * e^(-t_alive / buoyancy_const)
-        buoyancy_a = params["buoyancy"] * math.exp(-time_alive / params["buoyancy_const"])
-        return buoyancy_a
-    
-    # estimated time_alive for old particles by their location
-    # further in xy plane is colder than higher in z, estimate time by d/v
-    # buoyancy_a = buoyancy * e^(-sqrt(x^2 + y^2) / τ) * e^(-0.5 * z / τ), τ = buoyancy_const * v
-    v_xy_plane = math.sqrt(particle["X"][1]**2 + particle["Y"][1]**2)
-    tau_xy_plane = params["buoyancy_const"] * v_xy_plane
-    tau_z = params["buoyancy_const"] * np.abs(particle["Z"][1])
-    d_xy_plane = math.sqrt(particle["X"][0]**2 + particle["Y"][0]**2)
-    buoyancy_a = params["buoyancy"] * math.exp(-(d_xy_plane / tau_xy_plane) - (particle["Z"][0] / tau_z))
+def _get_buoyancy_acceleration(current_time, particle_idx, params):
+    time_emitted = particle_idx / params["particles_per_sec"]
+    time_alive = current_time - time_emitted
+    buoyancy_a = params["buoyancy"] * math.exp(-time_alive / params["buoyancy_const"])
     return buoyancy_a
 
 
-def _update_acceleration(particle, params, delta_t=None):
-    g = _get_gravity_acceleration()
-    drag_a = _get_drag_acceleration(particle, params)
-    buoyancy_a = _get_buoyancy_acceleration(particle, params, delta_t)
-
-    particle["Z"][2] += g
-    particle["Z"][2] += buoyancy_a
-    particle["X"][2] += drag_a[0]
-    particle["Y"][2] += drag_a[1]
-
-
 def _update_velocity(particle, delta_t):
-    # V(t) = V0 + a*t
-    particle["X"][1] += particle["X"][2] * delta_t
-    particle["Y"][1] += particle["Y"][2] * delta_t
-    particle["Z"][1] += particle["Z"][2] * delta_t
+    axes = ["X", "Y", "Z"]
+    for axis in axes:
+        # V(t) = V0 + a*t
+        particle[axis][1] += particle[axis][2] * delta_t
 
 
 # initial_velocity (x,y,z) [m/sec]
 def _update_location(particle, initial_v, delta_t):
-    # X(t) = X0 + V0*t + 0.5*a*t^2
-    particle["X"][0] += initial_v[0] * delta_t + 0.5 * particle["X"][2] * delta_t ** 2
-    particle["Y"][0] += initial_v[1] * delta_t + 0.5 * particle["Y"][2] * delta_t ** 2
-    particle["Z"][0] += initial_v[2] * delta_t + 0.5 * particle["Z"][2] * delta_t ** 2
+    axes = ["X", "Y", "Z"]
+    for idx, axis in enumerate(axes):
+        # X(t) = X0 + V0*t + 0.5*a*t^2
+        particle[axis][0] += initial_v[idx] * delta_t + 0.5 * particle[axis][2] * delta_t ** 2
+
+
+def _create_particle(current_time, particle_idx, params):
+    rng = _get_rng_particle_seed(current_time, particle_idx)
+    initial_loc = _get_random_chimney_loc(rng, params)
+    initial_v = _get_random_initial_velocity(rng, params)
+
+    particle = {
+        "X": [initial_loc[0], initial_v[0], 0],
+        "Y": [initial_loc[1], initial_v[1], 0],
+        "Z": [initial_loc[2], initial_v[2], 0]
+    }
+
+    return particle
+
 
 # create unique seed to each particle, for consistent results with random authentic look
 def _get_rng_particle_seed(current_time, particle_number):
     seed = int(current_time * 1000) + particle_number
     rng = np.random.default_rng(seed)
     return rng
+
 
 # use rng_seed of specific particle from _get_rng_particle_seed
 def _get_random_chimney_loc(rng_seed, params):
@@ -132,18 +123,9 @@ def _get_random_chimney_loc(rng_seed, params):
     y = radius * np.sin(theta)
     return [x, y, 0]
 
+
 # use rng_seed of specific particle from _get_rng_particle_seed
 def _get_random_initial_velocity(rng_seed, params):
     # random gaussian distribution
     v = rng_seed.normal(loc=params["ini_vel"], scale=params["vel_std"])
     return v
-
-
-def _create_particle(initial_loc, initial_v):
-    particle = {
-        "X": [initial_loc[0], initial_v[0], 0],
-        "Y": [initial_loc[1], initial_v[1], 0],
-        "Z": [initial_loc[2], initial_v[2], 0]
-    }
-
-    return particle
